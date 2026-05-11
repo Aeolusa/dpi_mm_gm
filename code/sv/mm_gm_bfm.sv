723333cd489f21cf0c2481ea215c1c8b829381e3
tpu <tp791696008@gmail.com>
2026-05-11 15:26:10 +0800

    update bfm

--- code/sv/mm_gm_bfm.sv
+++ code/sv/mm_gm_bfm.sv
@@ -1,27 +1,42 @@
+  
+module mm_gm_top #(
+    parameter REQ_W                                     = 1,
+    parameter TXRSP_CHNS                                = 1,
+    parameter TXRSP_W                                   = 1,
+    parameter RXRSP_CHNS                                = 1,
+    parameter RXRSP_W                                   = 1, 
+    parameter TXDAT_CHNS                                = 1,
+    parameter TXDAT_W                                   = 1,
+    parameter RXDAT_CHNS                                = 1,
+    parameter RXDAT_W                                   = 1 
+) (
+    input bit                                           clk,
+    input bit                                           rstn,
 
-
-
-module mm_gm_bfm (
-    
+    input logic [REQ_W-1:0]                             txreq_flit,
+    input logic                                         txreq_flitv,
+    input logic [TXRSP_CHNS-1:0][TXRSP_W-1:0]           txrsp_flit,
+    input logic [TXRSP_CHNS-1:0]                        txrsp_flitv,
+    input logic [RXRSP_CHNS-1:0][RXRSP_W-1:0]           rxrsp_flit,
+    input logic [RXRSP_CHNS-1:0]                        rxrsp_flitv,
+    input logic [TXDAT_CHNS-1:0][TXDAT_W-1:0]           txdat_flit,
+    input logic [TXDAT_CHNS-1:0]                        txdat_flitv,
+    input logic [RXDAT_CHNS-1:0][RXDAT_W-1:0]           rxdat_flit,
+    input logic [RXDAT_CHNS-1:0]                        rxdat_flitv           
 );
 
-    // ---- 初始化、请求、响应的接口均无变化 ----
-    import "DPI-C" function void refmodel_init(input int strict_mode);
-
     import "DPI-C" function void dpi_chi_txreq_write(
-        input int src_id, 
-        input int tgt_id, 
-        input int txn_id,
+        input int txnid,
         input longint addr, 
         input int size, 
-        input int burst_len,
+        input int opcode,
+        input int secvec,
         input longint req_time
     );
 
     import "DPI-C" function void dpi_chi_rxrsp_dbid(
-        input int src_id, 
-        input int tgt_id, 
-        input int txn_id, 
+        input int txnid, 
+        input int opcode,
         input int dbid
     );
 
@@ -31,31 +46,162 @@ module mm_gm_bfm (
 
     // 发送写数据 flit (原生 256-bit data + 32-bit byte_en)
     import "DPI-C" function void dpi_chi_txdat(
-        input int src_id, 
-        input int tgt_id, 
-        input int dbid,
-        input bit [255:0] data_bits,   // 直接传宽总线变量！
-        input bit [31:0]  byte_en_bits, // 直接传宽总线变量！
-        input int offset, 
-        input longint resp_time
+        input int txnid,
+        input int opcode,
+        input int dataid,   
+        input bit [255:0] data, 
+        input int be, 
+        input int data_cnt
     );
 
-    // 验证读数据 (原生变长 bit 数组)
-    // 这里的 resp_data_bits 需要声明为足够大的宽位，
-    // 比如单次最大支持512-bit，你可以写 input bit [511:0] resp_data_bits。
-    // C++ 底层会根据你传入的 size * burst_len 自动计算并只读取对应的有效字节！
-    import "DPI-C" function int dpi_chi_read_check(
-        input int src_id, 
-        input int tgt_id, 
-        input int txn_id,
-        input longint addr, 
-        input int size, 
-        input int burst_len,
-        input bit [511:0] resp_data_bits, // 或者 [255:0]，取决于你设定的最大单次传输范围
-        input longint req_time, 
-        input longint resp_time
+    import "DPI-C" function void dpi_chi_rxdat(
+        input int txnid,
+        input int opcode,
+        input int dataid,   
+        input bit [255:0] data, 
+        input int be, 
+        input int data_cnt
     );
 
+    parameter TXNID_W                                   = 12;
+    parameter REQOPCODE_W                               = 4;
+    parameter SIZE_W                                    = 4;
+    parameter SECVEC_W                                  = 4;
+    parameter ADDR_W                                    = 54;
+    parameter RSPOPCODE_W                               = 3;
+    parameter DBID_W                                    = 12;
+    parameter DATOPCODE_W                               = 2;
+    parameter DATAID_W                                  = 4;
+    parameter BE_W                                      = 32;
 
+    typedef struct packed {
+        logic [TXNID_W-1:0]     txnid;
+        logic [OPCODE_W-1:0]    opcode;
+        logic [ADDR_W-1:0]      addr;
+        logic [SECVEC_W-1:0]    secvec;
+        logic [SIZE_W-1:0]      size;
+    } req_t;
+
+    typedef struct packed {
+        logic [TXNID_W-1:0]     txnid;
+        logic [RSPOPCODE_W-1:0] opcode;
+        logic [DBID_W-1:0]      dbid;
+    } rsp_t;
+
+    typedef struct packed {
+        logic [TXNID_W-1:0]     txnid;
+        logic [DATOPCODE_W-1:0] opcode;
+        logic [DATAID_W-1:0]    dataid;
+        logic [BE_W-1:0]        be;
+        logic [255:0]           data;
+        logic [5:0]             data_cnt;
+    } dat_t;
+
+    function req_t req2dpi(input logic [REQ_W-1:0] req_flit);
+        req_t req;
+        req.txn_id  = req_flit[`REQ_TXNID];
+        req.opcode  = req_flit[`REQ_OPCODE];
+        req.addr    = req_flit[`REQ_ADDR];
+        req.secvec  = req_flit[`REQ_SECVEC];
+        req.size    = req_flit[`REQ_SIZE];
+        return req;
+    endfunction
+
+    function rsp_t rsp2dpi(input logic [RXRSP_W-1:0] rsp_flit);
+        rsp_t rsp;
+        rsp.txnid   = rsp_flit[`RSP_TXNID];
+        rsp.opcode  = rsp_flit[`RSP_OPCODE];
+        rsp.dbid    = rsp_flit[`RSP_DBID];
+        return rsp;
+    endfunction
+
+    function dat_t dat2dpi(input logic [TXDAT_W-1:0] dat_flit);
+        dat_t dat;
+        dat.txnid       = dat_flit[`DAT_TXNID];
+        dat.opcode      = dat_flit[`DAT_OPCODE];
+        dat.dataid      = dat_flit[`DAT_DATAID];
+        dat.be          = dat_flit[`DAT_BE];
+        dat.data        = dat_flit[`DAT_DATA];
+        dat.data_cnt    = dat_flit[`DAT_DATA_CNT];
+        return dat;
+    endfunction
+
+    req_t txreq;
+    rsp_t rxrsp[RXRSP_CHNS-1:0];
+    dat_t txdat[TXDAT_CHNS-1:0];
+    dat_t rxdat[RXDAT_CHNS-1:0];
+
+    always_comb begin
+        txreq = '0;
+        if (txreq_flitv) begin
+            txreq = req2dpi(txreq_flit);
+        end
+    end
+
+    for (genvar i = 0; i < RXRSP_CHNS; i++) begin
+        always_comb begin
+            rxrsp[i] = '0;
+            if (rxrsp_flitv[i]) begin
+                rxrsp[i] = rsp2dpi(rxrsp_flit[i]);
+            end
+        end
+
+        always_ff @(posedge clk) begin
+            if (rxrsp_flitv[i]) begin
+                dpi_chi_rxrsp_dbid(rxrsp[i].txnid, rxrsp[i].opcode, rxrsp[i].dbid);
+            end
+        end
+    end
+
+    for (genvar i = 0; i < TXDAT_CHNS; i++) begin
+        always_comb begin
+            txdat[i] = '0;
+            if (txdat_flitv[i]) begin
+                txdat[i] = dat2dpi(txdat_flit[i]);
+            end
+        end
+
+        always_ff @(posedge clk) begin
+            if (txdat_flitv[i]) begin
+                dpi_chi_txdat(txdat[i].txnid,
+                              txdat[i].opcode,
+                              txdat[i].dataid,
+                              txdat[i].data,
+                              txdat[i].be,
+                              txdat[i].data_cnt);
+            end
+        end
+    end
+
+    for (genvar i = 0; i < RXDAT_CHNS; i++) begin
+        always_comb begin
+            rxdat[i] = '0;
+            if (rxdat_flitv[i]) begin
+                rxdat[i] = dat2dpi(rxdat_flit[i]);
+            end
+        end
+
+        always_ff @(posedge clk) begin
+            if (rxdat_flitv[i]) begin
+                dpi_chi_rxdat(rxdat[i].txnid,
+                              rxdat[i].opcode,
+                              rxdat[i].dataid,
+                              rxdat[i].data,
+                              rxdat[i].be,
+                              rxdat[i].data_cnt);
+            end
+        end
+    end
+
+    always_ff @(posedge clk) begin
+        if (txreq_flitv) begin
+            dpi_chi_txreq_write(txreq.txnid, 
+                                txreq.addr, 
+                                txreq.size, 
+                                txreq.opcode, 
+                                txreq.secvec, 
+                                $time)
+        end
+    end
 
 endmodule
---
+++ code/sv/mm_gm_top.sv
@@ -0,0 +1,244 @@
+`define REQ_TXNID                                           26:15
+`define REQ_OPCODE                                          30:27
+`define REQ_SIZE                                            34:31
+`define REQ_SECVEC                                          38:35
+`define REQ_ADDR                                            92:39   
+`define RSP_TXNID                                           26:15
+`define RSP_OPCODE                                          29:27
+`define RSP_DBID                                            46:35    
+`define DAT_TXNID                                           26:15
+`define DAT_OPCODE                                          28:27
+`define DAT_DATAID                                          34:31
+`define DAT_BE                                              322:291
+`define DAT_DATA                                            290:35
+`define DAT_DATA_CNT                                        325:324         
+
+module mm_gm_top #(
+    parameter NID_WIDTH                                 = 11,
+    parameter SM_NUMS                                   = 8,
+    parameter SM_REQ_W                                  = 1, 
+    parameter SM_TXDAT_W                                = 1,
+    parameter SM_RXDAT_W                                = 1,
+    parameter SM_TXRSP_W                                = 1,
+    parameter SM_RXRSP_W                                = 1,
+    parameter SM_REQCHN_NUMS                            = 1,
+    parameter SM_TXRSPCHN_NUMS                          = 1,
+    parameter SM_RXRSPCHN_NUMS                          = 1,
+    parameter SM_TXDATCHN_NUMS                          = 1,
+    parameter SM_RXDATCHN_NUMS                          = 1,
+    parameter HST_REQ_W                                 = 1,
+    parameter HST_TXDAT_W                               = 1,
+    parameter HST_RXDAT_W                               = 1,
+    parameter HST_TXRSP_W                               = 1,
+    parameter HST_RXRSP_W                               = 1,
+    parameter HST_REQCHN_NUMS                           = 1,
+    parameter HST_TXRSPCHN_NUMS                         = 1,
+    parameter HST_RXRSPCHN_NUMS                         = 1,
+    parameter HST_TXDATCHN_NUMS                         = 1,
+    parameter HST_RXDATCHN_NUMS                         = 1,
+    parameter TS_REQ_W                                  = 1,
+    parameter TS_TXDAT_W                                = 1,
+    parameter TS_RXDAT_W                                = 1,
+    parameter TS_TXRSP_W                                = 1,
+    parameter TS_RXRSP_W                                = 1,
+    parameter TS_REQCHN_NUMS                            = 1,
+    parameter TS_TXRSPCHN_NUMS                          = 1,
+    parameter TS_RXRSPCHN_NUMS                          = 1,
+    parameter TS_TXDATCHN_NUMS                          = 1,
+    parameter TS_RXDATCHN_NUMS                          = 1,
+    parameter BLIT_REQ_W                                = 1,
+    parameter BLIT_TXDAT_W                              = 1,
+    parameter BLIT_RXDAT_W                              = 1,
+    parameter BLIT_TXRSP_W                              = 1,
+    parameter BLIT_RXRSP_W                              = 1,
+    parameter BLIT_REQCHN_NUMS                          = 1,
+    parameter BLIT_TXRSPCHN_NUMS                        = 1,
+    parameter BLIT_RXRSPCHN_NUMS                        = 1,
+    parameter BLIT_TXDATCHN_NUMS                        = 1,
+    parameter BLIT_RXDATCHN_NUMS                        = 1
+) (
+    input bit                                           sm_clk,
+    input bit                                           host_clk,
+    input bit                                           ts_clk,
+    input bit                                           blit_clk,
+    input bit                                           sm_rstn,
+    input bit                                           host_rstn,
+    input bit                                           ts_rstn,
+    input bit                                           blit_rstn,
+    // SM
+    input logic [SM_REQ_W-1:0]                          sm_txreq_flit,
+    input logic                                         sm_txreq_flitv,
+    input logic [SM_TXRSP_CHNS-1:0][SM_TXRSP_W-1:0]     sm_txrsp_flit,
+    input logic [SM_TXRSP_CHNS-1:0]                     sm_txrsp_flitv,
+    input logic [SM_RXRSP_CHNS-1:0][SM_RXRSP_W-1:0]     sm_rxrsp_flit,
+    input logic [SM_RXRSP_CHNS-1:0]                     sm_rxrsp_flitv,
+    input logic [SM_TXDAT_CHNS-1:0][SM_TXDAT_W-1:0]     sm_txdat_flit,
+    input logic [SM_TXDAT_CHNS-1:0]                     sm_txdat_flitv,
+    input logic [SM_RXDAT_CHNS-1:0][SM_RXDAT_W-1:0]     sm_rxdat_flit,
+    input logic [SM_RXDAT_CHNS-1:0]                     sm_rxdat_flitv,
+    // HOST          
+    input logic [HST_REQ_W-1:0]                         host_txreq_flit,
+    input logic                                         host_txreq_flitv,
+    input logic [HST_TXRSP_CHNS-1:0][HST_TXRSP_W-1:0]   host_txrsp_flit,
+    input logic [HST_TXRSP_CHNS-1:0]                    host_txrsp_flitv,
+    input logic [HST_RXRSP_CHNS-1:0][HST_RXRSP_W-1:0]   host_rxrsp_flit,
+    input logic [HST_RXRSP_CHNS-1:0]                    host_rxrsp_flitv,
+    input logic [HST_TXDAT_CHNS-1:0][HST_TXDAT_W-1:0]   host_txdat_flit,
+    input logic [HST_TXDAT_CHNS-1:0]                    host_txdat_flitv,
+    input logic [HST_RXDAT_CHNS-1:0][HST_RXDAT_W-1:0]   host_rxdat_flit,
+    input logic [HST_RXDAT_CHNS-1:0]                     host_rxdat_flitv,
+    // TS          
+    input logic [TS_REQ_W-1:0]                          ts_txreq_flit,
+    input logic                                         ts_txreq_flitv,
+    input logic [TS_TXRSP_CHNS-1:0][TS_TXRSP_W-1:0]     ts_txrsp_flit,
+    input logic [TS_TXRSP_CHNS-1:0]                     ts_txrsp_flitv,
+    input logic [TS_RXRSP_CHNS-1:0][TS_RXRSP_W-1:0]     ts_rxrsp_flit,
+    input logic [TS_RXRSP_CHNS-1:0]                     ts_rxrsp_flitv,
+    input logic [TS_TXDAT_CHNS-1:0][TS_TXDAT_W-1:0]     ts_txdat_flit,
+    input logic [TS_TXDAT_CHNS-1:0]                     ts_txdat_flitv,
+    input logic [TS_RXDAT_CHNS-1:0][TS_RXDAT_W-1:0]   ts_rxdat_flit,
+    input logic [TS_RXDAT_CHNS-1:0]                   ts_rxdat_flitv,
+    // BLIT          
+    input logic [BLIT_REQ_W-1:0]                        blit_txreq_flit,
+    input logic                                         blit_txreq_flitv,
+    input logic [BLIT_TXRSP_CHNS-1:0][BLIT_TXRSP_W-1:0] blit_txrsp_flit,
+    input logic [BLIT_TXRSP_CHNS-1:0]                   blit_txrsp_flitv,
+    input logic [BLIT_RXRSP_CHNS-1:0][BLIT_RXRSP_W-1:0] blit_rxrsp_flit,
+    input logic [BLIT_RXRSP_CHNS-1:0]                   blit_rxrsp_flitv,
+    input logic [BLIT_TXDAT_CHNS-1:0][BLIT_TXDAT_W-1:0] blit_txdat_flit,
+    input logic [BLIT_TXDAT_CHNS-1:0]                   blit_txdat_flitv,
+    input logic [BLIT_RXDAT_CHNS-1:0][BLIT_RXDAT_W-1:0] blit_rxdat_flit,
+    input logic [BLIT_RXDAT_CHNS-1:0]                   blit_rxdat_flitv
+);
+
+    // ---- 初始化、请求、响应的接口均无变化 ----
+    import "DPI-C" function void refmodel_init(input int strict_mode);
+
+    bit initial_m1;
+    bit initial_m2
+
+    always_ff @(posedge clk) begin
+        if (!sm_rstn) begin
+            initial_m1 <= 1'b1;
+        end else begin
+            initial_m1 <= 1'b0;
+        end
+    end
+
+    always_ff @(posedge clk) begin
+        initial_m2 <= initial_m1;
+    end
+
+    always_ff @(posedge clk) begin
+        if (!initial_m1 && initial_m2) begin
+            refmodel_init();
+        end
+    end
+
+    // SM BFM inst
+    for (genvar idx = 0; idx < SM_NUMS; idx++) begin : SM_BFM
+        mm_gm_bfm #(
+            .REQ_W                                  (SM_REQ_W),
+            .TXRSP_CHNS                             (SM_TXRSP_CHNS),
+            .TXRSP_W                                (SM_TXRSP_W),
+            .RXRSP_CHNS                             (SM_RXRSP_CHNS),
+            .RXRSP_W                                (SM_RXRSP_W),
+            .TXDAT_CHNS                             (SM_TXDAT_CHNS),
+            .TXDAT_W                                (SM_TXDAT_W),
+            .RXDAT_CHNS                             (SM_RXDAT_CHNS),
+            .RXDAT_W                                (SM_RXDAT_W)
+        ) u_sm_bfm (
+            .clk                                    (sm_clk),
+            .rstn                                   (sm_rstn),
+            .txreq_flit                             (sm_txreq_flit),
+            .txreq_flitv                            (sm_txreq_flitv),
+            .txrsp_flit                             (sm_txrsp_flit),
+            .txrsp_flitv                            (sm_txrsp_flitv),
+            .rxrsp_flit                             (sm_rxrsp_flit),
+            .rxrsp_flitv                            (sm_rxrsp_flitv),
+            .txdat_flit                             (sm_txdat_flit),
+            .txdat_flitv                            (sm_txdat_flitv),
+            .rxdat_flit                             (sm_rxdat_flit),
+            .rxdat_flitv                            (sm_rxdat_flitv)
+        );
+    end
+
+    // HOST BFM inst
+    mm_gm_bfm #(
+        .REQ_W                                  (HST_REQ_W),
+        .TXRSP_CHNS                             (HST_TXRSP_CHNS),
+        .TXRSP_W                                (HST_TXRSP_W),
+        .RXRSP_CHNS                             (HST_RXRSP_CHNS),
+        .RXRSP_W                                (HST_RXRSP_W),
+        .TXDAT_CHNS                             (HST_TXDAT_CHNS),
+        .TXDAT_W                                (HST_TXDAT_W),
+        .RXDAT_CHNS                             (HST_RXDAT_CHNS),
+        .RXDAT_W                                (HST_RXDAT_W)
+    ) u_hst_bfm (
+        .clk                                    (hst_clk),
+        .rstn                                   (hst_rstn),
+        .txreq_flit                             (hst_txreq_flit),
+        .txreq_flitv                            (hst_txreq_flitv),
+        .txrsp_flit                             (hst_txrsp_flit),
+        .txrsp_flitv                            (hst_txrsp_flitv),
+        .rxrsp_flit                             (hst_rxrsp_flit),
+        .rxrsp_flitv                            (hst_rxrsp_flitv),
+        .txdat_flit                             (hst_txdat_flit),
+        .txdat_flitv                            (hst_txdat_flitv),
+        .rxdat_flit                             (hst_rxdat_flit),
+        .rxdat_flitv                            (hst_rxdat_flitv)
+    );
+
+    // TS BFM inst
+    mm_gm_bfm #(
+        .REQ_W                                  (TS_REQ_W),
+        .TXRSP_CHNS                             (TS_TXRSP_CHNS),
+        .TXRSP_W                                (TS_TXRSP_W),
+        .RXRSP_CHNS                             (TS_RXRSP_CHNS),
+        .RXRSP_W                                (TS_RXRSP_W),
+        .TXDAT_CHNS                             (TS_TXDAT_CHNS),
+        .TXDAT_W                                (TS_TXDAT_W),
+        .RXDAT_CHNS                             (TS_RXDAT_CHNS),
+        .RXDAT_W                                (TS_RXDAT_W)
+    ) u_ts_bfm (
+        .clk                                    (ts_clk),
+        .rstn                                   (ts_rstn),
+        .txreq_flit                             (ts_txreq_flit),
+        .txreq_flitv                            (ts_txreq_flitv),
+        .txrsp_flit                             (ts_txrsp_flit),
+        .txrsp_flitv                            (ts_txrsp_flitv),
+        .rxrsp_flit                             (ts_rxrsp_flit),
+        .rxrsp_flitv                            (ts_rxrsp_flitv),
+        .txdat_flit                             (ts_txdat_flit),
+        .txdat_flitv                            (ts_txdat_flitv),
+        .rxdat_flit                             (ts_rxdat_flit),
+        .rxdat_flitv                            (ts_rxdat_flitv)
+    );
+
+    // BLIT BFM inst
+    mm_gm_bfm #(
+        .REQ_W                                  (BLIT_REQ_W),
+        .TXRSP_CHNS                             (BLIT_TXRSP_CHNS),
+        .TXRSP_W                                (BLIT_TXRSP_W),
+        .RXRSP_CHNS                             (BLIT_RXRSP_CHNS),
+        .RXRSP_W                                (BLIT_RXRSP_W),
+        .TXDAT_CHNS                             (BLIT_TXDAT_CHNS),
+        .TXDAT_W                                (BLIT_TXDAT_W),
+        .RXDAT_CHNS                             (BLIT_RXDAT_CHNS),
+        .RXDAT_W                                (BLIT_RXDAT_W)
+    ) u_blit_bfm (
+        .clk                                    (blit_clk),
+        .rstn                                   (blit_rstn),
+        .txreq_flit                             (blit_txreq_flit),
+        .txreq_flitv                            (blit_txreq_flitv),
+        .txrsp_flit                             (blit_txrsp_flit),
+        .txrsp_flitv                            (blit_txrsp_flitv),
+        .rxrsp_flit                             (blit_rxrsp_flit),
+        .rxrsp_flitv                            (blit_rxrsp_flitv),
+        .txdat_flit                             (blit_txdat_flit),
+        .txdat_flitv                            (blit_txdat_flitv),
+        .rxdat_flit                             (blit_rxdat_flit),
+        .rxdat_flitv                            (blit_rxdat_flitv)
+    );
+
+endmodule
